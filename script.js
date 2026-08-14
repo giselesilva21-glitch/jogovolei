337ef81ad7aedf8ffd9668cc1939e659aecae83b
giselesilva21-glitch <gisele.silva21@escola.pr.gov.br>
2026-08-14 10:48:34 -0300

    ok

---
+++ script.js
@@ -0,0 +1,14 @@
+function mostrarMensagem() {
+    alert("🏐 Bem-vindo ao Vôlei Mania! Confira nossas notícias e jogos.");
+}
+
+function lerNoticia(titulo) {
+    alert(
+        "📰 Você selecionou: " +
+        titulo +
+        "\n\nEm breve você poderá ler a notícia completa!"
+    );
+}
+
+// Mensagem no console
+console.log("🏐 Vôlei Mania carregado com sucesso!");
